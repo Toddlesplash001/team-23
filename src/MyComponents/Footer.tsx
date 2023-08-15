0c306c57de44ccbcc6304007e33e68f10de3d137
Toddlesplash001 <118872908+Toddlesplash001@users.noreply.github.com>
2023-08-15 12:21:46 +0000

    Making footer better

--- src/MyComponents/Footer.tsx
+++ src/MyComponents/Footer.tsx
@@ -1,22 +1,44 @@
 import React from "react";
 import "../MyComponents/Footer.css";
-
+import insta from "../assets/Instagram-Icon.png";
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="sb__footer section__padding">
+       <div className="sb__footer section__padding">
         <div className="sb__footer-links">
-          <div className="sb__footer-links_div">
+          <div className = "sb__footer-links_request"> 
             <a href = "https://forms.gle/Kc9ZYJenozVompAv5">
-            <h3>Didn't found your question? Ask here</h3>
+            <h3>Didn't find your question. Ask here</h3>
             </a>
-            <hr></hr>
-            <h4>Copyright @2023 All About Pec. All rights reserved.</h4>
-            
           </div>
+          <div className="sb__footer-links_div">
+            <br></br>
+            
+            <h4>Know its makers:</h4> 
+    
+            <a href = "https://www.instagram.com/kuwarjainn/">
+            <p><img src = {insta} alt = "" width = {15} height = {15}></img>  Kuwar Jain</p>
+            </a>
+            <a href = "https://www.instagram.com/tushar._sahni._/">
+            <p><img src = {insta} alt = "" width = {15} height = {15}></img>  Tushar Sahni</p>
+            </a>
+            <a href = "https://www.instagram.com/devanshvashisht_/">
+            <p><img src = {insta} alt = "" width = {15} height = {15}></img>  Devansh Vashisht</p>
+            </a>
+            </div>
+            
+            
+           
         </div>
+       <hr></hr>
+       <div className = "sb__footer-below">
+        <div className = "sb__footer-copyright">
+          <p>
+            @2023 All About PEC.All rights reserved.
+          </p>
+          </div>
+          </div> 
       </div>
-      
     </div>
   );
 };
